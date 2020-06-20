38fbe730644206a2e085e6390a51d997dddc90b1
rathna123 <mrreddy.testing@gmail.com>
2020-06-20 23:10:05 +0530

    Update SampleClass.java

--- SampleProject/src/SampleClass.java
+++ SampleProject/src/SampleClass.java
@@ -2,7 +2,7 @@
 public class SampleClass {
 
 	public static void main(String[] args) {
-		System.out.println(" master branch -thellow1");
+		System.out.println(" master branches -thellow1");
 	}
 
 }
