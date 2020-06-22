7b344fee166c72a99ca115eb746911e37a2753d4
rathna123 <mrreddy.testing@gmail.com>
2020-06-22 20:45:18 +0530

    Update SampleClass3.java

--- SampleProject/src/SampleClass3.java
+++ SampleProject/src/SampleClass3.java
@@ -4,6 +4,8 @@ public class SampleClass3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" mastr  - SampleClass 3");
+		System.out.println(" mastr  - SampleClass 33");
+		System.out.println(" mastr  - SampleClass 333");
 	}
 
 }
