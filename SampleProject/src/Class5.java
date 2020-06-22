af603ebcbc0fa565331d778f3dea30c7f6e2fb93
Rathna <mrathna.qa@gmail.com>
2020-06-22 20:15:16 +0530

    updated again

--- SampleProject/src/Class5.java
+++ SampleProject/src/Class5.java
@@ -3,7 +3,7 @@ public class Class5 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" B1 branches - SampleClass 3");
+		System.out.println(" B1111 branches - SampleClass 3");
 	}
 
 
