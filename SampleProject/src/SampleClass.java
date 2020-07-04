c7b6162325cbdbd8ce01d2e222bdd75125483100
Rathna <mrathna.qa@gmail.com>
2020-07-04 20:59:33 +0530

    cccc

--- SampleProject/src/SampleClass.java
+++ SampleProject/src/SampleClass.java
@@ -2,7 +2,7 @@
 public class SampleClass {
 
 	public static void main(String[] args) {
-		System.out.println(" master branches -thellow1");
+		System.out.println("ddd master branches -thellow1");
 	}
 
 }
