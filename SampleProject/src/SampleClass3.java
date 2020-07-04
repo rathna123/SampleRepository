cbc66d667a1fc4eab96f6798f3e7fcc204263a5f
Rathna <mrathna.qa@gmail.com>
2020-07-04 20:58:41 +0530

    hi

--- SampleProject/src/SampleClass3.java
+++ SampleProject/src/SampleClass3.java
@@ -4,7 +4,7 @@ public class SampleClass3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" mastr  - SampleClass 3");
-		System.out.println(" mastr  - SampleClass 3");
+		System.out.println(" mastrddddd - SampleClass 3");
 	}
 
 }
