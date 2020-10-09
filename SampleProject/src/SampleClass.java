0303c686314e466b18aba31c38fef86394426cb7
Rathna <mrathna.qa@gmail.com>
2020-10-09 09:20:30 +0530

    test

--- SampleProject/src/SampleClass.java
+++ SampleProject/src/SampleClass.java
@@ -4,6 +4,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println(" master branches -thellow1");
 		//remote merge test
+		//remote merge test again
 	}
 
 }
