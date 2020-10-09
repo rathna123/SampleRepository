d3aa04e6aa8677895bb5184657534f618672a6fb
Rathna <mrathna.qa@gmail.com>
2020-10-09 15:41:44 +0530

    mainmethod

--- SampleProject/src/Class5.java
+++ SampleProject/src/Class5.java
@@ -1,5 +1,12 @@
 // class 5
 public class Class5 {
-/// class 5--i m mastr
+/// class 5--i m mastr---555555
   //class-2
+	
+	//random
+	public void main(String args[])
+	{
+		System.out.println("hi");
+	}
+	
 }
