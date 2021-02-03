355ad7078f70480c85ff4decda67df41a40acbb6
Rathna <mrathna.qa@gmail.com>
2021-02-03 22:21:25 +0530

    hi

--- SampleProject/src/Class5.java
+++ SampleProject/src/Class5.java
@@ -3,7 +3,7 @@ public class Class5 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub ---- abbo class 5 ----remote test
-		System.out.println(" B11112222333----- branches - SampleClass 3");
+		System.out.println(" B11112222333----- branches - SampleClass 3fdf");
 	}
 
 
