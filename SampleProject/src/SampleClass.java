1237b8485d4b2c90a51e6e379a8dd77a6977e768
Rathna <mrathna.qa@gmail.com>
2021-02-07 21:24:28 +0530

    h

--- SampleProject/src/SampleClass.java
+++ SampleProject/src/SampleClass.java
@@ -2,7 +2,7 @@
 public class SampleClass {
 // class-1
 	public static void main(String[] args) {
-		System.out.println(" master branch -thellow123");
+		System.out.println(" master branch -thellow1223");
 		//remote merge test
 		//remote merge test again
 		//remote merge test again
