9ef4d4e564abb55996e49ce3e114424e232c0d38
Rathna <mrathna.qa@gmail.com>
2021-02-08 08:48:28 +0530

    h

--- SampleProject/src/SampleClass2.java
+++ SampleProject/src/SampleClass2.java
@@ -1,11 +1,5 @@
 
 public class SampleClass2 {
-//sm2
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//new commentsabc
-		//new comments=2
-
-	}
+hellow
 
 }
---
+++ SampleProject/src/SampleClass4.java
@@ -0,0 +1,12 @@
+
+public class SampleClass3 {
+/////hi
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" mastr  - I am a Class3");
+		System.out.println(" mastr  - I am a Class33");
+		System.out.println(" mastr  - I am a Classyyyyyyyyyyyyyyymaster");
+		//new comments
+	}
+
+}
